f9464762c11f9e2f2049fd75faea80c3d6547401
Kuekuatsheu <Kuekuatsheu@DESKTOP-PNP6TE2>
2023-04-23 20:45:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Enter a number: ");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.Write("Enter another number: ");
+double num2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("What do you want to do with these 2 numbers? Press;\n+ to add them\n- to subtract them\n* to multiply\n/ To divide");
+string operation = Console.ReadLine();
+
+switch (operation)
+{
+        case "+":
+        Console.WriteLine($"{num1}+{num2}={num1+num2}");
+        break;
+        case "-":
+        Console.WriteLine($"{num1}-{num2}={num1 - num2}");
+        break;
+        case "*":
+        Console.WriteLine($"{num1}*{num2}={num1 * num2}");
+        break;
+        case "/":
+        if(num2 != 0)
+        {
+            Console.WriteLine($"{num1}/{num2}={num1 / num2}");
+        }
+        else
+        {
+            Console.WriteLine("A number can not be divided to zero.");
+            break;
+        }
+        break;
+        default:
+        break;
+
+}
